6db71e8c0b8085e7d988f37d2725e89df228990d
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-07-08 18:55:33 +0200

    added SearchBar, fixed bugs, grouped everything in App

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import Business from '../Business/Business';
+import './BusinessList.css';
 
 class BusinessList extends React.Component {
 	render() {
-		<div className="BusinessList">
-			{Business}
-			{Business}
-			{Business}
-			{Business}
-			{Business}
-			{Business}
-		</div>;
+		return (
+			<div className="BusinessList">
+				<Business />
+				<Business />
+				<Business />
+				<Business />
+				<Business />
+				<Business />
+			</div>
+		);
 	}
 }
 
